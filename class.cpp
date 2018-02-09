cadbc9b238ed4213edc847cd68aa0dd72f2f59b5
ElonaFoobar <elonafoobar@gmail.com>
2018-02-09 15:05:20 +0900

    Delete unneccessary assignment statements

--- class.cpp
+++ class.cpp
@@ -57,14 +57,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"戦士は己の肉体を武器に、立ちはだかるものを破壊し突き進みます。魔法の扱いには向いていませんが、その高い戦闘能力には目を見張るものがあります。"s,
                 u8"The Warrior. With working knowledge of all things stabby, bashy, choppy, and shooty, anything in a Warrior's hands is bad news for their enemies. No mere thug, they have knowledge of tactics to bring even the strongest down, and are trained early on to be comfortable in all kinds of armor and shields. Warriors are personifications of the adage \"If violence isn't working, you are obviously not using enough of it.\""s);
-            p(0) = 10;
-            p(1) = 10;
-            p(2) = 2;
-            p(3) = 0;
-            p(4) = 0;
-            p(5) = 3;
-            p(6) = 0;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -118,14 +110,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"遺跡荒らしは、財宝を目当てに墓や遺跡から貴重品を持ち出すことで生計をたてています。彼らにはそこそこの戦闘能力と、商人と駆け引きをするために必要な才能があります。"s,
                 u8"The Thief. Good with swords and bows, though not a believer of a fair fight. Prefers to avoid taking hits if possible. Can dual wield if the weapons aren't too heavy. Years of dealing with fences have given the Thief skills with negotiating a better deal. A firm believer in the adage \"What's yours is mine, and what's mine is mine. See that guy over there, his stuff? Mine too.\""s);
-            p(0) = 4;
-            p(1) = 4;
-            p(2) = 8;
-            p(3) = 5;
-            p(4) = 4;
-            p(5) = 0;
-            p(6) = 0;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -176,14 +160,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"魔法使いは、その名のとおり魔法の知識に長けています。魔法使いの生死は、魔法書の取得と管理にかかっています。"s,
                 u8"The Wizard. Master of the Arcane, the Eldritch, and the Esoteric. A pity all those years have left little time to build muscles or quick reflexes. Still, they do have some small skill with staff and short sword. And while they start out so feeblePurits scare them, with a few books under their belt, and a little practice, a Wizard will soon decimate legions. Their motto: \"If it's not on fire now, it will be soon\"."s);
-            p(0) = 0;
-            p(1) = 0;
-            p(2) = 0;
-            p(3) = 4;
-            p(4) = 3;
-            p(5) = 8;
-            p(6) = 10;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -233,14 +209,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"農民は生産を得意とする職業です。彼らは我慢強く、またある程度の身体能力を持っています。"s,
                 u8"The Farmer. While not a steely-thewed Warrior or or well read Mage, the farmer is a Jack-of All-Trades. Good with a polearm from threshing grain, and good bargainers with their deals with merchants, they have working knowledge of just about every trade there is. As such, they never go hungry, as there's always something someone will pay dearly to get done. Their motto: \"Anything worth doing is best done yourself\"."s);
-            p(0) = 5;
-            p(1) = 5;
-            p(2) = 2;
-            p(3) = 0;
-            p(4) = 8;
-            p(5) = 5;
-            p(6) = 0;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -288,14 +256,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
         {
             classname = u8"プレデター"s;
             buff = lang(""s, ""s);
-            p(0) = 8;
-            p(1) = 11;
-            p(2) = 8;
-            p(3) = 0;
-            p(4) = 0;
-            p(5) = 0;
-            p(6) = 0;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -337,14 +297,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"狩人は遠隔攻撃のスペシャリストです。多少の打撃能力もありますが、大抵は敵の接近を許さずに倒すことができるでしょう。"s,
                 u8"The Archer. Skilled in the ways of the forest and hunting, they are master marksmen, and don't see why one should hunt monster any different than a deer. Skilled with the Short Sword, the axe, and of course all types of bows, plus a few skills they picked up on their long trips in the woods. Firm believers that \"Anything dead over THERE will not be bothering me HERE\"."s);
-            p(0) = 6;
-            p(1) = 4;
-            p(2) = 8;
-            p(3) = 5;
-            p(4) = 2;
-            p(5) = 0;
-            p(6) = 0;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -395,14 +347,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"魔法戦士は戦士と魔法使いの中間的な職業で、より打撃に重点を置いています。魔法を使いこなすためにも、バランスをとり成長させることが必要でしょう。"s,
                 u8"The Warmage. Not really a Wizard, not really a Warrior, the Warmage is a believer that stabbing things that are on fire is better than just stabbing them or setting them on fire. Good with swords, armor, books and casting. Their only weakness is that without specialization, they won't truely excel in a field. Still when your motto is: \"Everything worth doing is worth overdoing\", you don't really care."s);
-            p(0) = 6;
-            p(1) = 6;
-            p(2) = 2;
-            p(3) = 0;
-            p(4) = 0;
-            p(5) = 4;
-            p(6) = 7;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -452,14 +396,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"観光客はこの世界における伝統的な職業で、予想を裏切らないほど軟弱です。"s,
                 u8"The Tourist. They have no strengths, they have no weaknesses. They don't have anything really. Oh they can fish, I'll give you that, but if you want a real challenge to build something from the ground up, this is the way to go. After all, you have to teach them everything. Only for those who have a good amount of time to spare. You can even write your own motto if you want."s);
-            p(0) = 0;
-            p(1) = 0;
-            p(2) = 0;
-            p(3) = 0;
-            p(4) = 0;
-            p(5) = 0;
-            p(6) = 0;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -502,14 +438,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"ピアニストは演奏を生業とする孤高の職業です。彼らは常にピアノを背負っているため、重荷に耐える能力があります。"s,
                 u8"The Pianist. Where the Farmer has all the practical skills, the Pianist has all the impractical ones. Still, they can read a book, play a tune, make jewelry, and since they are expected to haul a piano around, they get real good with weight lifting. Their Motto: \"The whole world is my stage!\" And they believe it too."s);
-            p(0) = 6;
-            p(1) = 0;
-            p(2) = 4;
-            p(3) = 5;
-            p(4) = 6;
-            p(5) = 0;
-            p(6) = 4;
-            p(7) = 8;
         }
         if (dbmode == 9)
         {
@@ -556,14 +484,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
         {
             classname = u8"機工兵"s;
             buff = lang(""s, ""s);
-            p(0) = 0;
-            p(1) = 2;
-            p(2) = 5;
-            p(3) = 8;
-            p(4) = 5;
-            p(5) = 4;
-            p(6) = 3;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
@@ -609,14 +529,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"神官は魔法の能力と供に、若干の肉弾戦への適正を持っています。彼らは信仰する神から、最大限の恩恵を受けることができるでしょう。"s,
                 u8"The Priest. The Faithful, the devout. They bring the words of their God to the world, one battered heretic at a time. Good with all armors, shields, divine magic, blunt weapons, and reading. The Priest is a handful, though depending on their God, they may have hidden pratfalls as well. If the motto: \"A hymn a day,, will keep me away, Sinner!\" appeals to you, choose this."s);
-            p(0) = 2;
-            p(1) = 2;
-            p(2) = 0;
-            p(3) = 2;
-            p(4) = 2;
-            p(5) = 10;
-            p(6) = 7;
-            p(7) = 2;
         }
         if (dbmode == 9)
         {
@@ -666,14 +578,6 @@ int access_class_info(int dbmode, const std::string& dbidn)
             buff = lang(
                 u8"クレイモアは半人半妖の血が流れる俊敏な剣士です。銀眼の斬殺者と呼ばれる彼女達は、身の丈ほどの長さの大剣を自在に扱います。耐久力は低めですが、軽いみのこなしと敏感な目で、相手の攻撃を軽々とかわすことができます。"s,
                 u8"The Claymore. Half-human, with silver eyes and inhuman evasive capabilities, the Claymores are a mostly female class, as the males have… problems. Their weapon of choice is the Claymore, hence their name, and they prefer light armor, relying on their great speed and skills of evasion to protect them. They heal fast, but the healing comes at a price. Their bodies are unstable, their motto unknown."s);
-            p(0) = 9;
-            p(1) = 3;
-            p(2) = 7;
-            p(3) = 6;
-            p(4) = 0;
-            p(5) = 0;
-            p(6) = 4;
-            p(7) = 0;
         }
         if (dbmode == 9)
         {
