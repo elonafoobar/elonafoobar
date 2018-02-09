2cdb201bade15e4125b153f0c9fa37c4d39e6529
ElonaFoobar <elonafoobar@gmail.com>
2018-02-09 14:30:27 +0900

    Align data files
    Too many spaces for alignment may cause very big diff.

--- lang/en/race.lua
+++ lang/en/race.lua
@@ -1,317 +1,318 @@
-race = {}
+race = race or {}
+
 race.kobolt = {
-  name        = "Kobolt",
+  name = "Kobolt",
   description = "",
 }
 race.orc = {
-  name        = "Orc",
+  name = "Orc",
   description = "",
 }
 race.troll = {
-  name        = "Troll",
+  name = "Troll",
   description = "",
 }
 race.lizardman = {
-  name        = "Lizardman",
+  name = "Lizardman",
   description = "",
 }
 race.minotaur = {
-  name        = "Minotaur",
+  name = "Minotaur",
   description = "",
 }
 race.yerles = {
-  name        = "Yerles",
+  name = "Yerles",
   description =
   "The young kingdom which worships the ancient scientific civilization called Eyth Terre has gained power in the west. While they are realists amd materialistic, they are also adaptable to things that are beyond their comprehension if it benefits the kingdom. Yerles fit well in every class with their outstanding learning ability. They receive extra skill points with every level that they gain.",
 }
 race.norland = {
-  name        = "Norland",
+  name = "Norland",
   description = "",
 }
 race.eulderna = {
-  name        = "Eulderna",
+  name = "Eulderna",
   description =
   "Eulderna is one of the most ancient races in Sierra Terre. Egoism and elitism dominate every thought behind actions of these perfectionists. They, are suitable to caster or hybrid classes as they are adept at magics and using magical devices. Euldernas have resistance to magic.",
 }
 race.fairy = {
-  name        = "Fairy",
+  name = "Fairy",
   description =
   "Fairies are cute, mysterious, and fragile. Compared to the human race, they are gifted with superior skills of using magic and evading. In contrast, the lack of physical power makes it difficult for them to carry even the smallest of things, making it impossible for them to wear equipment weighing more than 1s. Fairies have outstanding resistances to all elements.",
 }
 race.asura = {
-  name        = "Asura",
+  name = "Asura",
   description = "",
 }
 race.slime = {
-  name        = "Slime",
+  name = "Slime",
   description = "",
 }
 race.wolf = {
-  name        = "Wolf",
+  name = "Wolf",
   description = "",
 }
 race.dwarf = {
-  name        = "Dwarf",
+  name = "Dwarf",
   description =
   "A Dwarf is a child of the earth. As tough and unyielding as the mountains they love, most Dwarves will live their whole lives underground, mining, smelting, and smithing. Some few leave the depths to become adventurers, hoping to make enough coin to start their own clan. Good with technology, and tough against things dark and poisonous.",
 }
 race.juere = {
-  name        = "Juere",
+  name = "Juere",
   description =
   "The Juere are a people wild and free. Scorning the \"weaker\" laws of others, they quickly learn the tricks and technologies of others before ever moving onwards. Their pride makes them great performers, and are used to being hungry for long periods of time. Nimble of hand, and Bold of character, no two Juere are ever the same.",
 }
 race.zombie = {
-  name        = "Zombie",
+  name = "Zombie",
   description = "",
 }
 race.elea = {
-  name        = "Elea",
+  name = "Elea",
   description =
   "Elea, known by the other peoples of the world as \"Elf's\", live quietly in their forests, far from man. Nimble and graceful, their bows are of legend, and their wizards have forgotten more of magic than many will ever know. Still, the young ones from time to time leave and go on adventures, if only to fill their memories for their long lives.",
 }
 race.rabbit = {
-  name        = "Rabbit",
+  name = "Rabbit",
   description = "",
 }
 race.sheep = {
-  name        = "Sheep",
+  name = "Sheep",
   description = "",
 }
 race.frog = {
-  name        = "Frog",
+  name = "Frog",
   description = "",
 }
 race.centipede = {
-  name        = "Centipede",
+  name = "Centipede",
   description = "",
 }
 race.snail = {
-  name        = "Snail",
+  name = "Snail",
   description =
   "Despite the fears that one day the rats will inherit the World, thanks to the Etherwind it seems the snails have gotten a head start. Slow and dull, even the mightiest among them fears salt, garlic and butter. Still for those seeking a true challenge, ony can harly do worse than being a snail. Can you imagine the looks of others when you inch ever so slowly towards them?",
 }
 race.mandrake = {
-  name        = "Mandrake",
+  name = "Mandrake",
   description = "",
 }
 race.beetle = {
-  name        = "Beetle",
+  name = "Beetle",
   description = "",
 }
 race.mushroom = {
-  name        = "Mushroom",
+  name = "Mushroom",
   description = "",
 }
 race.bat = {
-  name        = "Bat",
+  name = "Bat",
   description = "",
 }
 race.ent = {
-  name        = "Ent",
+  name = "Ent",
   description = "",
 }
 race.lich = {
-  name        = "Lich",
+  name = "Lich",
   description =
   "When a powerful magus grows near death, their desire to live on makes them attempt lichdom. Of the many who try every century, barely a handful succeed. Most fail and die, but some few partially make it, though the process robs them of much of their memories and skills. Their young \"age\" is their new \"life\" beginning anew, but they are patient. After all, they have all the time in the world now.",
 }
 race.hound = {
-  name        = "Hound",
+  name = "Hound",
   description = "",
 }
 race.ghost = {
-  name        = "Ghost",
+  name = "Ghost",
   description = "",
 }
 race.spirit = {
-  name        = "Spirit",
+  name = "Spirit",
   description = "",
 }
 race.eye = {
-  name        = "Eye",
+  name = "Eye",
   description = "",
 }
 race.wyvern = {
-  name        = "Wyvern",
+  name = "Wyvern",
   description = "",
 }
 race.wasp = {
-  name        = "Wasp",
+  name = "Wasp",
   description = "",
 }
 race.giant = {
-  name        = "Giant",
+  name = "Giant",
   description = "",
 }
 race.imp = {
-  name        = "Imp",
+  name = "Imp",
   description = "",
 }
 race.hand = {
-  name        = "Hand",
+  name = "Hand",
   description = "",
 }
 race.snake = {
-  name        = "Snake",
+  name = "Snake",
   description = "",
 }
 race.drake = {
-  name        = "Drake",
+  name = "Drake",
   description = "",
 }
 race.goblin = {
-  name        = "Goblin",
+  name = "Goblin",
   description =
   "If Dwarves are the people of the earth, then the Goblins are the vermin. While clumsy and ugly, there are ever so many goblins about, and more every year. Their scrawny bodies heal quickly, and they love the taste of fish. Their beady little eyes miss little in the dark, and you never want to grapple with them, who knows what diseases you might get.",
 }
 race.bear = {
-  name        = "Bear",
+  name = "Bear",
   description = "",
 }
 race.armor = {
-  name        = "Armor",
+  name = "Armor",
   description = "",
 }
 race.medusa = {
-  name        = "Medusa",
+  name = "Medusa",
   description = "",
 }
 race.cupid = {
-  name        = "Cupid",
+  name = "Cupid",
   description = "",
 }
 race.phantom = {
-  name        = "Phantom",
+  name = "Phantom",
   description = "",
 }
 race.harpy = {
-  name        = "Harpy",
+  name = "Harpy",
   description = "",
 }
 race.dragon = {
-  name        = "Dragon",
+  name = "Dragon",
   description = "",
 }
 race.dinosaur = {
-  name        = "Dinosaur",
+  name = "Dinosaur",
   description = "",
 }
 race.cerberus = {
-  name        = "Cerberus",
+  name = "Cerberus",
   description = "",
 }
 race.spider = {
-  name        = "Spider",
+  name = "Spider",
   description = "",
 }
 race.golem = {
-  name        = "Golem",
+  name = "Golem",
   description =
   "While certainly not a natural creature, the flows of the Etherwind have ingrained into them a spark of sentience. Strong as a team of oxen, and as tough as the rock they were sculpted from, those are pretty much the only strengths of a Golem. The wizards that made them wanted only laborers, not poets. Still, a Golem can slowly get better, and thankfully they lack the wit to even beome bored doing so.",
 }
 race.rock = {
-  name        = "Rock",
+  name = "Rock",
   description = "",
 }
 race.crab = {
-  name        = "Crab",
+  name = "Crab",
   description = "",
 }
 race.skeleton = {
-  name        = "Skeleton",
+  name = "Skeleton",
   description = "",
 }
 race.piece = {
-  name        = "Piece",
+  name = "Piece",
   description = "",
 }
 race.cat = {
-  name        = "Cat",
+  name = "Cat",
   description = "",
 }
 race.dog = {
-  name        = "Dog",
+  name = "Dog",
   description = "",
 }
 race.roran = {
-  name        = "Roran",
+  name = "Roran",
   description = "",
 }
 race.rat = {
-  name        = "Rat",
+  name = "Rat",
   description = "",
 }
 race.shell = {
-  name        = "Shell",
+  name = "Shell",
   description = "",
 }
 race.catgod = {
-  name        = "Catgod",
+  name = "Catgod",
   description = "",
 }
 race.machinegod = {
-  name        = "Machinegod",
+  name = "Machinegod",
   description = "",
 }
 race.undeadgod = {
-  name        = "Undeadgod",
+  name = "Undeadgod",
   description = "",
 }
 race.machine = {
-  name        = "Machine",
+  name = "Machine",
   description = "",
 }
 race.wisp = {
-  name        = "Wisp",
+  name = "Wisp",
   description = "",
 }
 race.chicken = {
-  name        = "Chicken",
+  name = "Chicken",
   description = "",
 }
 race.stalker = {
-  name        = "Stalker",
+  name = "Stalker",
   description = "",
 }
 race.catsister = {
-  name        = "Catsister",
+  name = "Catsister",
   description = "",
 }
 race.mutant = {
-  name        = "Mutant",
+  name = "Mutant",
   description =
   "A living warning of the perils of Gene-Engineering, Chaos, and why pregnant women should not be caught in the Etherwind, most mutants live short and pain-filled lives. Still, one in every thousand not only lives but thrives due to the changes wrought on them. Healing quickly due to their quickened blood, and at least starting out mostly human-like, Mutants are the proof that a third hand is always helpful.",
 }
 race.yeek = {
-  name        = "Yeek",
+  name = "Yeek",
   description = "",
 }
 race.yith = {
-  name        = "Yith",
+  name = "Yith",
   description = "",
 }
 race.servant = {
-  name        = "Servant",
+  name = "Servant",
   description = "",
 }
 race.horse = {
-  name        = "Horse",
+  name = "Horse",
   description = "",
 }
 race.god = {
-  name        = "God",
+  name = "God",
   description = "",
 }
 race.quickling = {
-  name        = "Quickling",
+  name = "Quickling",
   description = "",
 }
 race.metal = {
-  name        = "Metal",
+  name = "Metal",
   description = "",
 }
 race.bike = {
-  name        = "Bike",
+  name = "Bike",
   description = "",
 }
 
--- lang/jp/race.lua
+++ lang/jp/race.lua
@@ -1,317 +1,318 @@
-race = {}
+race = race or {}
+
 race.kobolt = {
-  name        = "コボルト",
+  name = "コボルト",
   description = "",
 }
 race.orc = {
-  name        = "オーク",
+  name = "オーク",
   description = "",
 }
 race.troll = {
-  name        = "トロール",
+  name = "トロール",
   description = "",
 }
 race.lizardman = {
-  name        = "リザードマン",
+  name = "リザードマン",
   description = "",
 }
 race.minotaur = {
-  name        = "ミノタウロス",
+  name = "ミノタウロス",
   description = "",
 }
 race.yerles = {
-  name        = "イェルス",
+  name = "イェルス",
   description =
   "シエラ・テール西方で急速に台頭してきた新王国の人間が、イェルスです。彼らは現実的な思考を好み、エイス・テールの文明を崇拝する一方で、それが例え自らの理解を超えていても、利益になるものであれば積極的にとりいれようとする柔軟性を持っています。彼らは高い学習能力を持ち、ほぼ全ての職業に適応することができます。イェルスは機械武装の扱いにたけ、追加の成長ボーナスを受け取ります。",
 }
 race.norland = {
-  name        = "ノーランド",
+  name = "ノーランド",
   description = "",
 }
 race.eulderna = {
-  name        = "エウダーナ",
+  name = "エウダーナ",
   description =
   "エウダーナは最も古い国の一つで、今もなお強力な大国です。彼らは、自分たちのみが世界を動かすに値すると考える選民意識の持ち主で、エリートであり、完ぺき主義者です。エウダーナは魔法や魔法道具の扱いにたけ、キャスターや中間的な職業に向いています。彼らは高い詠唱能力と魔法への耐性をもっています。",
 }
 race.fairy = {
-  name        = "妖精",
+  name = "妖精",
   description =
   "妖精は可憐で非力で儚い生物です。彼女達は、人間族と比べて高い魔力と回避力、目にも止まらぬすばしっこさを持ち、ほぼ全ての耐性に高い抵抗力を持っています。反面、妖精は重さ1s以上の装備を身につけることができず、防御力をあげるのは困難です。その体力の低さからも、まともな物理攻撃を一度でもくらうと危機に陥るでしょう。",
 }
 race.asura = {
-  name        = "阿修羅",
+  name = "阿修羅",
   description = "",
 }
 race.slime = {
-  name        = "スライム",
+  name = "スライム",
   description = "",
 }
 race.wolf = {
-  name        = "狼",
+  name = "狼",
   description = "",
 }
 race.dwarf = {
-  name        = "丘の民",
+  name = "丘の民",
   description =
   "丘の民、別名ドワーフは頑強で無骨な背の低い種族です。彼らは高い耐久力と力を持ち、幾つかの生産系の技術にも精通しています。彼らには暗黒と強い毒への耐性があります。",
 }
 race.juere = {
-  name        = "ジューア",
+  name = "ジューア",
   description =
   "ジューアは、元は盗賊や悪人のあつまりであり、今もならず者国家として嫌われる国です。彼らは束縛されることを嫌い、自分の意思を尊重し、自由に生きることを好みます。ジューアは手先が器用で、鋭い感覚と高い学習能力を持ち、多彩な技術に精通しています。彼らはパフォーマンスをすることで生計をたてることができ、食事をゆっくりと消化し、採取スポットでより長い時間採取できます。",
 }
 race.zombie = {
-  name        = "ゾンビ",
+  name = "ゾンビ",
   description = "",
 }
 race.elea = {
-  name        = "エレア",
+  name = "エレア",
   description =
   "エレアは、幾つもの世代を生き抜いたエルフの末梢で、他の種族から姿を隠し、静かに生活しています。彼らこそが、エイス・テールを滅ぼした災厄の元凶であるとするイェルスの皇子のバッシングにより、彼らは他の種族から嫌われています。エレアはエーテルへの耐性と高い魔力を持ち、魔力の反動によるダメージを軽減することができます。",
 }
 race.rabbit = {
-  name        = "ウサギ",
+  name = "ウサギ",
   description = "",
 }
 race.sheep = {
-  name        = "羊",
+  name = "羊",
   description = "",
 }
 race.frog = {
-  name        = "蛙",
+  name = "蛙",
   description = "",
 }
 race.centipede = {
-  name        = "ムカデ",
+  name = "ムカデ",
   description = "",
 }
 race.snail = {
-  name        = "かたつむり",
+  name = "かたつむり",
   description =
   "カタツムリは非常に鈍い生き物です。彼らは脆く、非力で、不器用で、学習能力がなく、塩で溶けます。",
 }
 race.mandrake = {
-  name        = "マンドレイク",
+  name = "マンドレイク",
   description = "",
 }
 race.beetle = {
-  name        = "昆虫",
+  name = "昆虫",
   description = "",
 }
 race.mushroom = {
-  name        = "きのこ",
+  name = "きのこ",
   description = "",
 }
 race.bat = {
-  name        = "コウモリ",
+  name = "コウモリ",
   description = "",
 }
 race.ent = {
-  name        = "エント",
+  name = "エント",
   description = "",
 }
 race.lich = {
-  name        = "リッチ",
+  name = "リッチ",
   description =
   "リッチは高い知能と魔力を備えた不朽のアンデッドです。彼らは通常の種族よりも高いマナと瞑想の技術を得ますが、肉弾戦に耐え切れるほどの体力はありません。リッチは冷気と毒と強い暗黒への耐性を持っています。",
 }
 race.hound = {
-  name        = "猟犬",
+  name = "猟犬",
   description = "",
 }
 race.ghost = {
-  name        = "幽霊",
+  name = "幽霊",
   description = "",
 }
 race.spirit = {
-  name        = "精霊",
+  name = "精霊",
   description = "",
 }
 race.eye = {
-  name        = "目",
+  name = "目",
   description = "",
 }
 race.wyvern = {
-  name        = "ワイバーン",
+  name = "ワイバーン",
   description = "",
 }
 race.wasp = {
-  name        = "ワスプ",
+  name = "ワスプ",
   description = "",
 }
 race.giant = {
-  name        = "巨人",
+  name = "巨人",
   description = "",
 }
 race.imp = {
-  name        = "インプ",
+  name = "インプ",
   description = "",
 }
 race.hand = {
-  name        = "手",
+  name = "手",
   description = "",
 }
 race.snake = {
-  name        = "蛇",
+  name = "蛇",
   description = "",
 }
 race.drake = {
-  name        = "ドレイク",
+  name = "ドレイク",
   description = "",
 }
 race.goblin = {
-  name        = "ゴブリン",
+  name = "ゴブリン",
   description =
   "ゴブリンは好戦的で獰猛な種族です。彼らは特筆するべき能力を持っておらず、またこれといった弱点もありません。ゴブリンははやや高い能力と、暗黒への耐性と、自然治癒の技術を持っています。また、採取スポットでより長く採取することができます。",
 }
 race.bear = {
-  name        = "熊",
+  name = "熊",
   description = "",
 }
 race.armor = {
-  name        = "鎧",
+  name = "鎧",
   description = "",
 }
 race.medusa = {
-  name        = "メデューサ",
+  name = "メデューサ",
   description = "",
 }
 race.cupid = {
-  name        = "キューピット",
+  name = "キューピット",
   description = "",
 }
 race.phantom = {
-  name        = "ファントム",
+  name = "ファントム",
   description = "",
 }
 race.harpy = {
-  name        = "ハーピー",
+  name = "ハーピー",
   description = "",
 }
 race.dragon = {
-  name        = "ドラゴン",
+  name = "ドラゴン",
   description = "",
 }
 race.dinosaur = {
-  name        = "恐竜",
+  name = "恐竜",
   description = "",
 }
 race.cerberus = {
-  name        = "ケルベロス",
+  name = "ケルベロス",
   description = "",
 }
 race.spider = {
-  name        = "蜘蛛",
+  name = "蜘蛛",
   description = "",
 }
 race.golem = {
-  name        = "ゴーレム",
+  name = "ゴーレム",
   description =
   "ゴーレムは魔力により生命を与えられた物質です。ゴーレムは他の種族に比べ遥かに高い体力を持っています。その代償としてか、彼らのマナと速度は、通常よりも劣っています。ゴーレムには毒への強い耐性があり、また朦朧状態にならないという利点があります。",
 }
 race.rock = {
-  name        = "岩",
+  name = "岩",
   description = "",
 }
 race.crab = {
-  name        = "蟹",
+  name = "蟹",
   description = "",
 }
 race.skeleton = {
-  name        = "骸骨",
+  name = "骸骨",
   description = "",
 }
 race.piece = {
-  name        = "駒",
+  name = "駒",
   description = "",
 }
 race.cat = {
-  name        = "猫",
+  name = "猫",
   description = "",
 }
 race.dog = {
-  name        = "犬",
+  name = "犬",
   description = "",
 }
 race.roran = {
-  name        = "ローラン",
+  name = "ローラン",
   description = "",
 }
 race.rat = {
-  name        = "ねずみ",
+  name = "ねずみ",
   description = "",
 }
 race.shell = {
-  name        = "やどかり",
+  name = "やどかり",
   description = "",
 }
 race.catgod = {
-  name        = "猫の神",
+  name = "猫の神",
   description = "",
 }
 race.machinegod = {
-  name        = "機械の神",
+  name = "機械の神",
   description = "",
 }
 race.undeadgod = {
-  name        = "死霊の神",
+  name = "死霊の神",
   description = "",
 }
 race.machine = {
-  name        = "機械",
+  name = "機械",
   description = "",
 }
 race.wisp = {
-  name        = "ウィスプ",
+  name = "ウィスプ",
   description = "",
 }
 race.chicken = {
-  name        = "鶏",
+  name = "鶏",
   description = "",
 }
 race.stalker = {
-  name        = "ストーカー",
+  name = "ストーカー",
   description = "",
 }
 race.catsister = {
-  name        = "妹猫",
+  name = "妹猫",
   description = "",
 }
 race.mutant = {
-  name        = "カオスシェイプ",
+  name = "カオスシェイプ",
   description =
   "カオスシェイプについて分かっていることは、ほとんどありません。異形の人とも呼ばれる彼らは、かろうじて人の形を保っています。カオスシェイプは成長とともに新しい身体の部位を獲得しますが、何が生えてくるかは予測不可能です。",
 }
 race.yeek = {
-  name        = "イーク",
+  name = "イーク",
   description = "",
 }
 race.yith = {
-  name        = "イス",
+  name = "イス",
   description = "",
 }
 race.servant = {
-  name        = "神の化身",
+  name = "神の化身",
   description = "",
 }
 race.horse = {
-  name        = "馬",
+  name = "馬",
   description = "",
 }
 race.god = {
-  name        = "神",
+  name = "神",
   description = "",
 }
 race.quickling = {
-  name        = "クイックリング",
+  name = "クイックリング",
   description = "",
 }
 race.metal = {
-  name        = "メタル",
+  name = "メタル",
   description = "",
 }
 race.bike = {
-  name        = "バイク",
+  name = "バイク",
   description = "",
 }
 
