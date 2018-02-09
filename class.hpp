438bfb40e51f70942bedd586ed0e0d8569ab06d3
ElonaFoobar <elonafoobar@gmail.com>
2018-02-09 15:16:34 +0900

    Externalize class data

--- class.hpp
+++ class.hpp
@@ -1,11 +1,39 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 #include <vector>
+#include "optional_ref.hpp"
 
 
 namespace elona
 {
+
+
+struct class_data
+{
+    std::string id;
+    bool is_extra;
+    int item_type;
+};
+
+
+
+class class_db
+{
+public:
+    class_db();
+
+
+    optional_ref<class_data> operator[](const std::string& id) const;
+
+
+private:
+    std::unordered_map<std::string, class_data> storage;
+};
+
+
+inline class_db the_class_db;
 
 
 inline int cequipment = 0;
---
+++ data/class.lua
@@ -0,0 +1,50 @@
+define("warrior", {
+  item_type = 1,
+  is_extra = false,
+})
+define("thief", {
+  item_type = 7,
+  is_extra = false,
+})
+define("wizard", {
+  item_type = 2,
+  is_extra = false,
+})
+define("farmer", {
+  item_type = 1,
+  is_extra = false,
+})
+define("predator", {
+  item_type = 0,
+  is_extra = true,
+})
+define("archer", {
+  item_type = 3,
+  is_extra = false,
+})
+define("warmage", {
+  item_type = 5,
+  is_extra = false,
+})
+define("tourist", {
+  item_type = 0,
+  is_extra = false,
+})
+define("pianist", {
+  item_type = 3,
+  is_extra = false,
+})
+define("gunner", {
+  item_type = 4,
+  is_extra = true,
+})
+define("priest", {
+  item_type = 6,
+  is_extra = false,
+})
+define("claymore", {
+  item_type = 8,
+  is_extra = false,
+})
+
+-- vim: et sw=2 sts=2
