857ab9e03e16c2b89bfa5711bac4e09641f35746
ElonaFoobar <elonafoobar@gmail.com>
2018-02-10 08:34:20 +0900

    Use the unique random generator in game

---
+++ random.cpp
@@ -0,0 +1,45 @@
+#include "random.hpp"
+#include <algorithm>
+
+
+namespace elona
+{
+
+
+void randomize(std::random_device::result_type seed)
+{
+    detail::random_engine.seed(seed);
+}
+
+
+void exrand_randomize(std::random_device::result_type seed)
+{
+    detail::random_engine2.seed(seed);
+}
+
+
+
+void exrand_rnd(int& result, int max)
+{
+    if (max <= 0)
+    {
+        result = 0;
+    }
+    else
+    {
+        result =
+            std::uniform_int_distribution<>{0, max - 1}(detail::random_engine2);
+    }
+}
+
+
+
+int rnd(int n)
+{
+    std::uniform_int_distribution<> dist{0,
+                                         std::clamp(n - 1, 0, (1 << 16) - 1)};
+    return dist(detail::random_engine);
+}
+
+
+} // namespace elona
