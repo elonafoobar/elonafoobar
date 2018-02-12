0775e99b42294ab40b54b3991613b5db31423e8c
ElonaFoobar <elonafoobar@gmail.com>
2018-02-12 17:31:31 +0900

    Fix BUG57

--- talk_npc.cpp
+++ talk_npc.cpp
@@ -933,7 +933,7 @@ void talk_npc()
             }
             arenaop(1) = cdata[rc].id;
             buff = lang(
-                u8"今日の対戦相手は"s + cdatan(0, rc) + _da() + u8"挑戦する"s
+                u8"今日の対戦相手は"s + refchara_str(cdata[rc].id, 2) + _da() + u8"挑戦する"s
                     + _noka(1),
                 u8"You got "s + cdatan(0, rc) + u8" today. What'ya say?"s);
         }
