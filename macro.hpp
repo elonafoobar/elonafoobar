057d64f88456d5b04cce91c93904ac7c70f3c4e0
ki-foobar <ki.foobar@gmail.com>
2018-03-17 03:20:07 +0900

    ELONA_TALK_SCENE_CUT and ELONA_APPEND_RESPONSE

--- macro.hpp
+++ macro.hpp
@@ -18,3 +18,28 @@
         ELONA_APPEND_PROMPT(i18n::_(u8"ui", u8"yes"), u8"y"s, u8"0"s); \
         ELONA_APPEND_PROMPT(i18n::_(u8"ui", u8"no"), u8"n"s, u8"1"s); \
     } while (0)
+
+
+
+#define ELONA_TALK_SCENE_CUT() \
+    do \
+    { \
+        talk_window(); \
+        if (scenemode) \
+        { \
+            if (scene_cut == 1) \
+            { \
+                talk_end(); \
+                return; \
+            } \
+        } \
+    } while (0)
+
+
+#define ELONA_APPEND_RESPONSE(x, y) \
+    do \
+    { \
+        list(0, listmax) = (x); \
+        listn(0, listmax) = (y); \
+        ++listmax; \
+    } while (0)
