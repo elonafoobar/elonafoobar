085cd96134b44a4c562ff90754dece1587093d1b
ki-foobar <ki.foobar@gmail.com>
2018-03-23 21:39:58 +0900

    Simplify enum class curse_state_t

--- enums.hpp
+++ enums.hpp
@@ -7,16 +7,16 @@ namespace elona
 
 enum class curse_state_t
 {
-    doomed = -2,
-    cursed = -1,
-    none = 0,
-    blessed = 1,
+    doomed,
+    cursed,
+    none,
+    blessed,
 };
 
 
 inline bool is_cursed(curse_state_t s)
 {
-    return s == curse_state_t::doomed || s == curse_state_t::cursed;
+    return s <= curse_state_t::cursed;
 }
 
 } // namespace elona
