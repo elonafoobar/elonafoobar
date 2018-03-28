bd5e619b32fbbcef7aeaf443a306b1f22fa6d33a
ki-foobar <ki.foobar@gmail.com>
2018-03-28 18:15:46 +0900

    Make quick save and load possible in other than wizard mode

--- elonacore.cpp
+++ elonacore.cpp
@@ -71550,22 +71550,6 @@ label_2747:
 
     if (gdata_wizard)
     {
-        if (getkey(snail::key::f1))
-        {
-            save_game();
-            txt(lang(u8" *保存* "s, u8" *Save* "s));
-            await(100);
-        }
-        if (getkey(snail::key::f2))
-        {
-            msg_newline();
-            msgtemp = u8"  "s;
-            firstturn = 1;
-            load_save_data();
-            mode = 3;
-            initialize_map();
-            return;
-        }
         if (getkey(snail::key::f5))
         {
             what_do_you_wish_for();
@@ -71596,6 +71580,22 @@ label_2747:
                 return;
             }
         }
+    }
+    if (getkey(snail::key::f1))
+    {
+        save_game();
+        txt(lang(u8" *保存* "s, u8" *Save* "s));
+        await(100);
+    }
+    if (getkey(snail::key::f2))
+    {
+        msg_newline();
+        msgtemp = u8"  "s;
+        firstturn = 1;
+        load_save_data();
+        mode = 3;
+        initialize_map();
+        return;
     }
     if (getkey(snail::key::f3))
     {
