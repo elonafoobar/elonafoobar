6d3bf3ca8a6d4102c30f2f4b8a87c91739705110
ki-foobar <ki.foobar@gmail.com>
2018-04-26 14:22:12 +0900

    Add struct version_t

--- version.hpp
+++ version.hpp
@@ -3,30 +3,48 @@
 
 namespace elona
 {
-namespace version
-{
 
 
 /* clang-format off */
 
-extern const int major;
-extern const int minor;
-extern const int patch;
-extern const char* revision;
-extern const char* timestamp;
-extern const char* platform;
 
-// Like 10203
-extern const int id;
+struct version_t
+{
+    int major;
+    int minor;
+    int patch;
+    std::string revision;
+    std::string timestamp;
+    std::string platform;
 
-// Like "1.2.3".
-extern const char* short_string;
+    // Like 10203
+    int id;
 
-// Like "Elona Foobar version 1.2.3 (14db8cb), compiled on Darwin-17.4.0 at 2018-04-15T14:07:28Z"
-extern const char* long_string;
+    // Like "1.2.3".
+    const char* short_string;
+
+    // Like "Elona Foobar version 1.2.3 (14db8cb), compiled on Darwin-17.4.0 at 2018-04-15T14:07:28Z"
+    const char* long_string;
+
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        ar(major);
+        ar(minor);
+        ar(patch);
+        ar(revision);
+        ar(timestamp);
+        ar(platform);
+    }
+>>>>>>> 57de873... TMP
+};
+
 
 /* clang-format on */
 
 
-} // namespace version
+extern const version_t latest_version;
+
+
 } // namespace elona
