9b495435216b4d5ba54680514335602f5468b2ff
Ian Pickering <ipickering2@gmail.com>
2018-05-14 14:32:52 -0700

    move two declarations

--- talk.hpp
+++ talk.hpp
@@ -25,6 +25,8 @@ void talk_window_init_and_show();
 void talk_window_init();
 void talk_window_show();
 void talk_window_query();
+int talk_check_trade(int = 0);
+int talk_guide_quest_client(int = 0);
 
 talk_result_t talk_more();
 talk_result_t talk_sleeping();
