1f1aa2fd8ff4d58ce64734f24858bad24c6690e6
Ian Pickering <ipickering2@gmail.com>
2018-08-10 19:46:38 -0700

    Add ui_menu_book

--- src/menu.cpp
+++ src/menu.cpp
@@ -25,6 +25,8 @@
 #include "ui.hpp"
 #include "variables.hpp"
 
+#include "ui/ui_menu_book.hpp"
+
 namespace elona
 {
 
@@ -7270,112 +7272,7 @@ label_2016_internal:
 
 void show_book_window()
 {
-    snd(59);
-    gsel(4);
-    pos(0, 0);
-    picload(filesystem::dir::graphic() / u8"book.bmp", 1);
-    gsel(0);
-    notesel(buff);
-    {
-        buff(0).clear();
-        std::ifstream in{(filesystem::dir::data() / u8"book.txt").native(),
-                         std::ios::binary};
-        std::string tmp;
-        while (std::getline(in, tmp))
-        {
-            buff(0) += tmp + '\n';
-        }
-    }
-    p = instr(buff, 0, ""s + inv[ci].param1 + u8","s + lang(u8"JP"s, u8"EN"s));
-    if (p == -1)
-    {
-        return;
-    }
-    buff = strmid(buff, p, instr(buff, p, u8"%END"s));
-    notedel(0);
-    listmax = 0;
-    page = 0;
-    pagesize = 40;
-    cs = 0;
-    cc = 0;
-    listmax = noteinfo();
-    keyrange = 0;
-    key_list(0) = key_enter;
-label_2023_internal:
-    cs_bk = -1;
-    pagemax = (listmax - 1) / pagesize;
-    if (page < 0)
-    {
-        page = pagemax;
-    }
-    else if (page > pagemax)
-    {
-        page = 0;
-    }
-    wx = (windoww - 720) / 2 + inf_screenx;
-    wy = winposy(468);
-    pos(wx, wy);
-    gcopy(4, 0, 0, 736, 448);
-    for (int cnt = 0, cnt_end = (pagesize); cnt < cnt_end; ++cnt)
-    {
-        p = pagesize * page + cnt;
-        if (p >= listmax)
-        {
-            break;
-        }
-        x = wx + 80 + cnt / 20 * 306;
-        y = wy + 45 + cnt % 20 * 16;
-        noteget(s, p);
-        if (p == 0)
-        {
-            font(12 + sizefix - en * 2, snail::font_t::style_t::bold);
-        }
-        if (p == 1)
-        {
-            font(10 + sizefix - en * 2);
-        }
-        if (p > 2)
-        {
-            font(12 + sizefix - en * 2);
-        }
-        pos(x, y);
-        mes(s);
-        if (p % 20 == 0)
-        {
-            font(12 + sizefix - en * 2, snail::font_t::style_t::bold);
-            pos(x + 90, y + 330);
-            mes(u8"- "s + (p / 20 + 1) + u8" -"s);
-        }
-    }
-label_2024_internal:
-    redraw();
-    await(config::instance().wait1);
-    key_check();
-    cursor_check();
-    if (key == key_pageup)
-    {
-        if (pagemax != 0)
-        {
-            snd(71);
-            ++page;
-            goto label_2023_internal;
-        }
-    }
-    if (key == key_pagedown)
-    {
-        if (pagemax != 0)
-        {
-            snd(71);
-            --page;
-            goto label_2023_internal;
-        }
-    }
-    if (key == key_cancel || key == key_enter)
-    {
-        update_screen();
-        return;
-    }
-    goto label_2024_internal;
+    ui::ui_menu_book(inv[ci].param1).show();
 }
 
 
