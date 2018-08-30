c49d12d24bcbbe7cb98c476bcf419d8f1551ddae
ki-foobar <ki.foobar@gmail.com>
2018-08-30 16:21:46 +0900

    Fix wish "alias"
    When you wish "alias", you were able to change your alias regardless of
    whether you are in wizard mode or not.

--- src/wish.cpp
+++ src/wish.cpp
@@ -318,19 +318,23 @@ bool grant_special_wishing(const std::string& wish)
         {
             txt(lang(u8"だめよ。", u8"*laugh*"));
         }
-        txt(lang(u8"新しい異名は？", u8"What's your new alias?"));
-        int stat = select_alias(0);
-        if (stat == 1)
-        {
-            txt(lang(
-                u8"あなたの新しい異名は「" + cmaka + u8"」。満足したかしら？",
-                u8"You will be known as <" + cmaka + u8">."));
-            cdatan(1, 0) = cmaka;
-        }
         else
         {
-            txt(lang(
-                u8"あら、そのままでいいの？", u8"What a waste of a wish!"));
+            txt(lang(u8"新しい異名は？", u8"What's your new alias?"));
+            int stat = select_alias(0);
+            if (stat == 1)
+            {
+                txt(lang(
+                    u8"あなたの新しい異名は「" + cmaka
+                        + u8"」。満足したかしら？",
+                    u8"You will be known as <" + cmaka + u8">."));
+                cdatan(1, 0) = cmaka;
+            }
+            else
+            {
+                txt(lang(
+                    u8"あら、そのままでいいの？", u8"What a waste of a wish!"));
+            }
         }
     }
     else if (
