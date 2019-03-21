32fb677ec54f4e1ec38308b9b644a43543965f64
ki-foobar <ki.foobar@gmail.com>
2019-03-21 11:39:22 +0900

    Add doc comment to util/either.hpp

--- src/util/either.hpp
+++ src/util/either.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
-#include <boost/optional.hpp>
 #include <boost/variant.hpp>
 
 
 
+/**
+ * Utility module for safe error handling.
+ * It is highly inspired by Haskell's Data.Either.
+ */
 namespace either
 {
 
@@ -29,6 +32,7 @@ struct right_holder
 
 
 
+/// Make a left value.
 template <typename T>
 detail::left_holder<T> left(T&& value)
 {
@@ -37,6 +41,7 @@ detail::left_holder<T> left(T&& value)
 
 
 
+/// Make a right value.
 template <typename T>
 detail::right_holder<T> right(T&& value)
 {
@@ -45,6 +50,13 @@ detail::right_holder<T> right(T&& value)
 
 
 
+/**
+ * Represents either left or right.
+ * Typically, left means failure, and right means success. It is because right
+ * is *right*.
+ * The template parameters, Left and Right, are wrapped by left_holder and
+ * right_holder because boost::variant cannot take the same type.
+ */
 template <typename Left, typename Right>
 struct either
     : public boost::
@@ -71,6 +83,7 @@ public:
 
 
 
+    /// Constructs with the left value.
     either(const left_holder& value)
         : base_type(value)
     {
@@ -78,6 +91,7 @@ public:
 
 
 
+    /// Move-constructs with the left value.
     either(left_holder&& value)
         : base_type(value)
     {
@@ -85,6 +99,7 @@ public:
 
 
 
+    /// Constructs with the right value.
     either(const right_holder& value)
         : base_type(value)
     {
@@ -92,6 +107,7 @@ public:
 
 
 
+    /// Move-constructs with the right value.
     either(right_holder&& value)
         : base_type(value)
     {
@@ -99,6 +115,7 @@ public:
 
 
 
+    /// Factory method to build a left value.
     static either left_of(const left_type& left)
     {
         return either(left_holder{left});
@@ -106,6 +123,7 @@ public:
 
 
 
+    /// Factory method to build a left value.
     static either left_of(left_type&& left)
     {
         return either(left_holder{std::move(left)});
@@ -113,6 +131,7 @@ public:
 
 
 
+    /// Factory method to build a right value.
     static either right_of(const right_type& right)
     {
         return either(right_holder{right});
@@ -120,6 +139,7 @@ public:
 
 
 
+    /// Factory method to build a right value.
     static either right_of(right_type&& right)
     {
         return either(right_holder{std::move(right)});
@@ -127,6 +147,7 @@ public:
 
 
 
+    /// Get which type the either has.
     which_type which() const
     {
         return base_type::which() == 0 ? which_type::left : which_type::right;
@@ -148,6 +169,7 @@ public:
 
 
 
+    /// Return true if it is right; otherwise false.
     explicit operator bool() const
     {
         return is_right();
@@ -155,12 +177,14 @@ public:
 
 
 
+    /// Get the left value.
     left_type& left()
     {
         return boost::get<left_holder>(*this).value;
     }
 
 
+    /// Get the left value.
     const left_type& left() const
     {
         return boost::get<left_holder>(*this).value;
@@ -168,12 +192,14 @@ public:
 
 
 
+    /// Get the right value.
     right_type& right()
     {
         return boost::get<right_holder>(*this).value;
     }
 
 
+    /// Get the right value.
     const right_type& right() const
     {
         return boost::get<right_holder>(*this).value;
