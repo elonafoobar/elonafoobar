dd9934945fcb124a841ce4e78084e7887da65620
Ian Pickering <ipickering2@gmail.com>
2019-05-19 13:53:46 -0700

    Remove dependency on Boost.Locale

--- src/util/filepathutil.cpp
+++ src/util/filepathutil.cpp
@@ -1,10 +1,10 @@
 #include "filepathutil.hpp"
 #include <algorithm>
-#include <boost/locale.hpp>
 #include <boost/predef.h>
 
 // For get_executable_path()
 #if BOOST_OS_WINDOWS
+#include <codecvt> // std::codecvt_utf8_utf16
 #include <Shlobj.h> // SHGetKnownFolderPath
 #include <windows.h> // GetModuleFileName
 #elif BOOST_OS_MACOS
@@ -29,28 +29,53 @@ namespace filepathutil
 {
 
 namespace fs = boost::filesystem;
-namespace conv = boost::locale::conv;
 
 
 
+/**
+ * Converts a UTF-8 encoded string to a filesystem path in the platform's native
+ * encoding (UTF-16 for Windows, UTF-8 for everything else)
+ */
 fs::path u8path(const std::string& str)
 {
-    return conv::utf_to_utf<fs::path::string_type::value_type>(str);
+#if BOOST_OS_WINDOWS
+    return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>()
+        .from_bytes(str);
+#else
+    return str;
+#endif
 }
 
 
 
+/**
+ * Returns a UTF-8 string from a filesystem path with all directory separators
+ * converted to the platform's preferred separators ('\' for Windows, '/' for
+ * everything else)
+ */
 std::string make_preferred_path_in_utf8(const fs::path& path)
 {
-    auto path_ = path;
-    return conv::utf_to_utf<char>(path_.make_preferred().native());
+#if BOOST_OS_WINDOWS
+    return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>()
+        .to_bytes(path.make_preferred().native());
+#else
+    return path.native();
+#endif
 }
 
 
 
+/**
+ * Returns a UTF-8 string from a filesystem path.
+ */
 std::string to_utf8_path(const fs::path& path)
 {
-    return conv::utf_to_utf<char>(path.native());
+#if BOOST_OS_WINDOWS
+    return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>()
+        .to_bytes(path.native());
+#else
+    return path.native();
+#endif
 }
 
 
@@ -109,7 +134,7 @@ boost::optional<std::string> get_home_directory()
     wchar_t* knownFolderPath;
 
     auto hr = SHGetKnownFolderPath(
-      FOLDERID_Profile, KF_FLAG_DEFAULT, nullptr, &knownFolderPath);
+        FOLDERID_Profile, KF_FLAG_DEFAULT, nullptr, &knownFolderPath);
 
     if (SUCCEEDED(hr))
     {
@@ -118,7 +143,7 @@ boost::optional<std::string> get_home_directory()
         home_directory = std::string(result);
         CoTaskMemFree(knownFolderPath);
     }
-  #elif BOOST_OS_LINUX || BOOST_OS_MACOS
+#elif BOOST_OS_LINUX || BOOST_OS_MACOS
     char* result = getenv("HOME");
     if (result == nullptr)
     {
