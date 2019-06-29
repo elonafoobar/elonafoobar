2afe95301c0497ca1405ef97774dcdde37ee6358
ki-foobar <ki.foobar@gmail.com>
2019-06-29 20:54:09 +0900

    Support data-update.lua

---
+++ src/tests/data/registry/load_order/init.lua
@@ -0,0 +1,19 @@
+local Data = Elona.require 'Data'
+
+print('----- init -----')
+
+do
+   local spell = Data.get('core.ability', 'core.healing')
+   print(spell)
+   local spells = Data.get_table('core.ability')
+   print(spells)
+end
+
+print('----------')
+
+-- do
+--    local spell = data.raw['core.ability']['core.healing']
+--    print(spell)
+--    local spells = data.raw['core.ability']
+--    print(spells)
+-- end
---
+++ src/tests/data/registry/load_order/mod.hcl
@@ -0,0 +1,9 @@
+mod {
+    id = "load_order"
+    name = "load_order"
+    author = "KI"
+    version = "0.1.0"
+    license = "MIT"
+    description = "Test mod."
+    dependencies = { core = "*" }
+}
