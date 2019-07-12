e1e63bd79fdf27ed0d8585f3fb3a397cac29eb56
ki-foobar <ki.foobar@gmail.com>
2019-07-13 00:32:03 +0900

    Don't save volatile data in Store object
    Volatile data fields has prefix, `_`, and are not serialized. This rule
    is only applied to top-level fields of Store because we have to save
    Handle object with its private fields such as `__index`.

--- runtime/data/script/kernel/serial.lua
+++ runtime/data/script/kernel/serial.lua
@@ -19,6 +19,26 @@ local function resolve_handle(value, seen)
    end
 end
 
+-- Does not mutate `value`.
+local function remove_volatile_data(value)
+   if type(value) ~= "table" then
+      return value
+   end
+
+   -- For table, remove volatile data.
+   local result = {}
+   for k, v in pairs(value) do
+      if type(k) == "string" and k:sub(1, 1) == "_" then
+         -- It's a private field, skipping.
+      else
+         -- This rule is only applied to top-level fields because we have to
+         -- save Handle object with its private fields such as `__index`.
+         result[k] = v
+      end
+   end
+   return result
+end
+
 resolve_handles = function(data, seen)
    for key, value in pairs(data) do
       resolve_handle(value, seen)
@@ -26,6 +46,7 @@ resolve_handles = function(data, seen)
 end
 
 Serial.save = function(s)
+   s = remove_volatile_data(s)
    local dump = serpent.dump(s)
    return dump
 end
