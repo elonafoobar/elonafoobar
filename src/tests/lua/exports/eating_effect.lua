457fcb5c3eb59b99e2be5d059a674eabc17effc2
ki-foobar <ki.foobar@gmail.com>
2019-07-25 21:47:47 +0900

    Reduce execution time to run all tests
    Debug build generated by MSVC is very slow, which makes us to re-run
    Appveyor CI manually due to timeout. This commit reduces
    `testing::start_in_debug_map()`'s calls. That is a bit risky to make it
    harder to detect regression, but slow CI execution also slows
    development.

--- src/tests/lua/exports/eating_effect.lua
+++ src/tests/lua/exports/eating_effect.lua
@@ -3,9 +3,9 @@ require "tests/lua/support/minctest"
 local Chara = Elona.require("Chara")
 local Exports = Elona.require("core", "Exports")
 
+Testing.start_in_debug_map()
 for name, func in pairs(Exports.eating_effect) do
    lrun("test " .. name, function()
-           Testing.start_in_debug_map()
-           lok(pcall(function() func(Chara.player()) end), "")
+      lok(pcall(function() func(Chara.player()) end), "")
    end)
 end
