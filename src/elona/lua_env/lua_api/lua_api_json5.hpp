1344441f089a0e1c60461cb4bf17d8d312b52f13
ki-foobar <ki.foobar@gmail.com>
2020-01-26 16:06:15 +0900

    API JSON5: JSON5.parse() returns (nil, error) instead of throwing error

--- src/elona/lua_env/lua_api/lua_api_json5.hpp
+++ src/elona/lua_env/lua_api/lua_api_json5.hpp
@@ -17,7 +17,9 @@ namespace lua
 namespace LuaApiJSON5
 {
 
-sol::object parse(const std::string& source, sol::this_state state);
+std::pair<sol::object, sol::optional<std::string>> parse(
+    const std::string& source,
+    sol::this_state state);
 std::string stringify(sol::object value, sol::table opts);
 
 void bind(sol::table&);
