1747c37a41ba87e6a0ca8ebedbf3f4a273cf0d20
ki-foobar <ki.foobar@gmail.com>
2020-02-23 11:20:34 +0900

    Do not use std::to_chars() for some environments
    macOS 10.14.x on Travis CI fails to execute the function and crashes at
    runtime.

--- src/elona/position.hpp
+++ src/elona/position.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <charconv>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 
@@ -32,28 +32,9 @@ struct Position
 
     std::string to_string() const
     {
-        // `int64_t` can be represented by at most 20 bytes. The string
-        // representation of `Position` consists of 2 integers and 4 additional
-        // punctuations, '(', ',', ' ' and ')', so 20+20+4+1=45 is enough (+1
-        // is NUL at the end of the string).
-        // As `std::to_chars()` never fails unless the buffer provided is
-        // smaller for the value, all calls of the function here will succeed.
-        char buf[45];
-        char* ptr = buf;
-
-        auto putc = [&](char c) { // Put one character on `buf`.
-            *ptr = c;
-            ++ptr;
-        };
-
-        putc('(');
-        ptr = std::to_chars(ptr, std::end(buf), x).ptr;
-        putc(',');
-        putc(' ');
-        ptr = std::to_chars(ptr, std::end(buf), y).ptr;
-        putc(')');
-        *ptr = '\0';
-        return std::string(buf, ptr);
+        std::stringstream ss;
+        ss << '(' << x << ", " << y << ')';
+        return ss.str();
     }
 
 
