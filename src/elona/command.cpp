df31a3c385476201ba34129ae3243562a8cdedf5
ki-foobar <ki.foobar@gmail.com>
2020-05-27 19:29:19 +0900

    Fix issue related to unlocking box (#1630)

--- src/elona/command.cpp
+++ src/elona/command.cpp
@@ -2931,8 +2931,7 @@ TurnResult do_open_command(Item& box, bool play_sound)
     {
         if (box.param2 != 0)
         {
-            int stat = unlock_box(box.param2);
-            if (stat == 0)
+            if (!try_unlock(box.param2))
             {
                 screenupdate = -1;
                 update_screen();
@@ -5964,13 +5963,13 @@ void proc_autopick()
 
 
 
-int unlock_box(int difficulty)
+bool try_unlock(int difficulty)
 {
     const auto lockpick_opt = item_find(636, 3);
     if (!lockpick_opt)
     {
         txt(i18n::s.get("core.action.unlock.do_not_have_lockpicks"));
-        return 0;
+        return false;
     }
     auto& lockpick = *lockpick_opt;
 
@@ -6020,14 +6019,13 @@ int unlock_box(int difficulty)
         txt(i18n::s.get("core.action.unlock.try_again"));
         if (yes_no())
         {
-            unlock_box(difficulty);
-            return 0;
+            return try_unlock(difficulty);
         }
-        return 0;
+        return false;
     }
     txt(i18n::s.get("core.action.unlock.succeed"));
     chara_gain_exp_lock_picking(cdata[cc]);
-    return 1;
+    return true;
 }
 
 
@@ -6352,8 +6350,7 @@ TurnResult try_to_open_locked_door()
     {
         if (cc == 0)
         {
-            int stat = unlock_box(feat(2));
-            if (stat == 0)
+            if (!try_unlock(feat(2)))
             {
                 screenupdate = -1;
                 update_screen();
