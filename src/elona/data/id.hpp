f3058761c6d3d69cf71d67b17fbd980433998cad
ki-foobar <ki.foobar@gmail.com>
2020-09-09 22:12:05 +0900

    Simplify implementation of PrototypeId and InstanceId

--- src/elona/data/id.hpp
+++ src/elona/data/id.hpp
@@ -6,218 +6,134 @@
 
 
 // Defines several kinds of strongly-typed IDs.
-namespace elona
+namespace elona::data
 {
-namespace data
+
+namespace detail
 {
+
+template <typename _Tag>
+struct _Id
+{
+    /// Constructs empty ID.
+    _Id() = default;
+
+
+    /// Constructs ID from @a id.
+    explicit _Id(SharedId id)
+        : _inner(id)
+    {
+    }
+
+
+    /// Constructs ID from @a id.
+    explicit _Id(const char* id)
+        : _inner(id)
+    {
+    }
+
+
+    /// Constructs ID from @a id.
+    explicit _Id(const std::string& id)
+        : _inner(id)
+    {
+    }
+
+
+    /// Constructs ID from @a static_id.
+    /// This implicit conversion ctor takes only char array with static size,
+    /// typically string literals.
+    template <size_t N>
+    _Id(const char (&static_id)[N])
+        : _inner(static_id)
+    {
+    }
+
+
+    /// Check equality with @a other.
+    bool operator==(const _Id& other) const noexcept
+    {
+        return _inner == other._inner;
+    }
+
+
+    /// Check equality with @a other.
+    bool operator!=(const _Id& other) const noexcept
+    {
+        return _inner != other._inner;
+    }
+
+
+    /// Converts itself to string.
+    const std::string& get() const noexcept
+    {
+        return _inner.get();
+    }
+
+
+    /// Calculates hash value of the ID.
+    size_t hash() const noexcept
+    {
+        return std::hash<SharedId>{}(_inner);
+    }
+
+
+
+    template <typename Archive>
+    void serialize(Archive& ar)
+    {
+        /* clang-format off */
+        if constexpr (serialization::concepts::is_iarchive_v<Archive>)
+        {
+            std::string tmp;
+            ar(tmp);
+            _inner = tmp;
+        }
+        else
+        {
+            std::string tmp = _inner.get();
+            ar(tmp);
+        }
+        /* clang-format on */
+    }
+
+
+
+private:
+    SharedId _inner;
+};
+
+
+
+struct _PrototypeIdTag
+{
+};
+
+struct _InstanceIdTag
+{
+};
+
+} // namespace detail
+
+
 
 /// Represents prototype ID.
-struct PrototypeId
-{
-    /// Constructs empty ID.
-    PrototypeId() = default;
-
-
-    /// Constructs ID from @a id.
-    explicit PrototypeId(SharedId id)
-        : _inner(id)
-    {
-    }
-
-
-    /// Constructs ID from @a id.
-    explicit PrototypeId(const char* id)
-        : _inner(id)
-    {
-    }
-
-
-    /// Constructs ID from @a id.
-    explicit PrototypeId(const std::string& id)
-        : _inner(id)
-    {
-    }
-
-
-    /// Constructs ID from @a static_id.
-    /// This implicit conversion ctor takes only char array with static size,
-    /// typically string literals.
-    template <size_t N>
-    PrototypeId(const char (&static_id)[N])
-        : _inner(static_id)
-    {
-    }
-
-
-    /// Check equality with @a other.
-    bool operator==(const PrototypeId& other) const noexcept
-    {
-        return _inner == other._inner;
-    }
-
-
-    /// Check equality with @a other.
-    bool operator!=(const PrototypeId& other) const noexcept
-    {
-        return _inner != other._inner;
-    }
-
-
-    /// Converts itself to string.
-    const std::string& get() const noexcept
-    {
-        return _inner.get();
-    }
-
-
-    /// Calculates hash value of the ID.
-    size_t hash() const noexcept
-    {
-        return std::hash<SharedId>{}(_inner);
-    }
-
-
-
-    template <typename Archive>
-    void serialize(Archive& ar)
-    {
-        /* clang-format off */
-        if constexpr (serialization::concepts::is_iarchive_v<Archive>)
-        {
-            std::string tmp;
-            ar(tmp);
-            _inner = tmp;
-        }
-        else
-        {
-            std::string tmp = _inner.get();
-            ar(tmp);
-        }
-        /* clang-format on */
-    }
-
-
-
-private:
-    SharedId _inner;
-};
-
-
+using PrototypeId = detail::_Id<detail::_PrototypeIdTag>;
 
 /// Represents instance ID.
-struct InstanceId
-{
-    /// Constructs empty ID.
-    InstanceId() = default;
+using InstanceId = detail::_Id<detail::_InstanceIdTag>;
 
-
-    /// Constructs ID from @a id.
-    explicit InstanceId(SharedId id)
-        : _inner(id)
-    {
-    }
-
-
-    /// Constructs ID from @a id.
-    explicit InstanceId(const char* id)
-        : _inner(id)
-    {
-    }
-
-
-    /// Constructs ID from @a id.
-    explicit InstanceId(const std::string& id)
-        : _inner(id)
-    {
-    }
-
-
-    /// Constructs ID from @a static_id.
-    /// This implicit conversion ctor takes only char array with static size,
-    /// typically string literals.
-    template <size_t N>
-    InstanceId(const char (&static_id)[N])
-        : _inner(static_id)
-    {
-    }
-
-
-    /// Check equality with @a other.
-    bool operator==(const InstanceId& other) const noexcept
-    {
-        return _inner == other._inner;
-    }
-
-
-    /// Check equality with @a other.
-    bool operator!=(const InstanceId& other) const noexcept
-    {
-        return _inner != other._inner;
-    }
-
-
-    /// Converts itself to string.
-    const std::string& get() const noexcept
-    {
-        return _inner.get();
-    }
-
-
-    /// Calculates hash value of the ID.
-    size_t hash() const noexcept
-    {
-        return std::hash<SharedId>{}(_inner);
-    }
-
-
-
-    template <typename Archive>
-    void serialize(Archive& ar)
-    {
-        /* clang-format off */
-        if constexpr (serialization::concepts::is_iarchive_v<Archive>)
-        {
-            std::string tmp;
-            ar(tmp);
-            _inner = tmp;
-        }
-        else
-        {
-            std::string tmp = _inner.get();
-            ar(tmp);
-        }
-        /* clang-format on */
-    }
-
-
-
-private:
-    SharedId _inner;
-};
-
-} // namespace data
-} // namespace elona
+} // namespace elona::data
 
 
 
 namespace std
 {
 
-template <>
-struct hash<::elona::data::PrototypeId>
+template <typename Tag>
+struct hash<::elona::data::detail::_Id<Tag>>
 {
-    size_t operator()(::elona::data::PrototypeId id) const
-    {
-        return id.hash();
-    }
-};
-
-
-
-template <>
-struct hash<::elona::data::InstanceId>
-{
-    size_t operator()(::elona::data::InstanceId id) const
+    size_t operator()(::elona::data::detail::_Id<Tag> id) const
     {
         return id.hash();
     }
