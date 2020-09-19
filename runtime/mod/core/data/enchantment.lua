b839b5cb722899e9d29422410c3b33410cd2ba3d
ki-foobar <ki.foobar@gmail.com>
2020-09-20 07:00:52 +0900

    Add 'core.enchantment' data

---
+++ runtime/mod/core/data/enchantment.lua
@@ -0,0 +1,150 @@
+ELONA.data:define_prototype("enchantment")
+ELONA.data:add(
+   "core.enchantment",
+   {
+      increases_attribute = {
+         legacy_id = 10000,
+      },
+      increases_resistance = {
+         legacy_id = 20000,
+      },
+      increases_skill = {
+         legacy_id = 30000,
+      },
+      maintains_attribute = {
+         legacy_id = 60000,
+      },
+      extra_damage = {
+         legacy_id = 70000,
+      },
+      invokes_effect = {
+         legacy_id = 80000,
+      },
+      special_ammo = {
+         legacy_id = 90000,
+      },
+      random_teleport = {
+         legacy_id = 21,
+      },
+      no_teleport = {
+         legacy_id = 22,
+      },
+      no_blindness = {
+         legacy_id = 23,
+      },
+      no_paralysis = {
+         legacy_id = 24,
+      },
+      no_confusion = {
+         legacy_id = 25,
+      },
+      no_fear = {
+         legacy_id = 26,
+      },
+      no_sleep = {
+         legacy_id = 27,
+      },
+      no_poison = {
+         legacy_id = 28,
+      },
+      seven_league_boots_effect = {
+         legacy_id = 29,
+      },
+      protects_from_etherwind = {
+         legacy_id = 30,
+      },
+      aurora_ring_effect = {
+         legacy_id = 31,
+      },
+      floating = {
+         legacy_id = 32,
+      },
+      protects_from_mutation = {
+         legacy_id = 33,
+      },
+      spell_enhancement = {
+         legacy_id = 34,
+      },
+      can_see_invisible = {
+         legacy_id = 35,
+      },
+      absorbs_stamina = {
+         legacy_id = 36,
+      },
+      ragnarok = {
+         legacy_id = 37,
+      },
+      absorbs_mp = {
+         legacy_id = 38,
+      },
+      absolute_piercing = {
+         legacy_id = 39,
+      },
+      time_stop = {
+         legacy_id = 40,
+      },
+      protects_from_thieves = {
+         legacy_id = 41,
+      },
+      can_digest_rotten = {
+         legacy_id = 42,
+      },
+      protects_from_curse = {
+         legacy_id = 43,
+      },
+      critical_hits = {
+         legacy_id = 44,
+      },
+      sucks_blood = {
+         legacy_id = 45,
+      },
+      disturbs_growth = {
+         legacy_id = 46,
+      },
+      attracts_monsters = {
+         legacy_id = 47,
+      },
+      protects_from_aliens = {
+         legacy_id = 48,
+      },
+      performance_rewards = {
+         legacy_id = 49,
+      },
+      extra_melee_attack = {
+         legacy_id = 50,
+      },
+      extra_ranged_attack = {
+         legacy_id = 51,
+      },
+      decreases_damage = {
+         legacy_id = 52,
+      },
+      nullifies_damage = {
+         legacy_id = 53,
+      },
+      cut_counterattack = {
+         legacy_id = 54,
+      },
+      diminishes_bleeding = {
+         legacy_id = 55,
+      },
+      gods_signal = {
+         legacy_id = 56,
+      },
+      slays_dragons = {
+         legacy_id = 57,
+      },
+      slays_undeads = {
+         legacy_id = 58,
+      },
+      reveals_religion = {
+         legacy_id = 59,
+      },
+      goulds_piano_effect = {
+         legacy_id = 60,
+      },
+      slays_gods = {
+         legacy_id = 61,
+      },
+   }
+)
