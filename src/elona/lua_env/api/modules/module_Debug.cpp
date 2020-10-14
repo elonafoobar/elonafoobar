b11ad9b8451750d7d9055ab1a17cd097757424f7
ki-foobar <ki.foobar@gmail.com>
2020-10-14 20:25:34 +0900

    Lua API: remove Debug.log/Debug.report_error
    Instead, use `Log.info()` and `Log.error()`.

--- src/elona/lua_env/api/modules/module_Debug.cpp
+++ src/elona/lua_env/api/modules/module_Debug.cpp
@@ -16,43 +16,6 @@ namespace elona::lua::api::modules::module_Debug
 {
 
 /**
- * @luadoc log
- *
- * Logs a message to log.txt.
- *
- * @tparam string message the message to log
- */
-void Debug_log(const std::string& message)
-{
-    ELONA_LOG("Lua.debug: " + message);
-}
-
-
-
-/**
- * @luadoc report_error
- *
- * Reports an error to the game console and log file.
- *
- * @tparam string message the error message to report
- */
-void Debug_report_error(const std::string& message)
-{
-    std::istringstream sstream(message);
-    std::string line;
-
-    txt("Script error: ", Message::color{ColorIndex::red});
-    while (getline(sstream, line, '\n'))
-    {
-        txt(line + "  ", Message::color{ColorIndex::red});
-    }
-
-    ELONA_ERROR("Debug.report_error: " + message);
-}
-
-
-
-/**
  * @luadoc enable_wizard
  *
  * Enable Wizard mode. It is equivalent to turn `core.wizard.is_enabled` on.
@@ -133,8 +96,6 @@ void bind(sol::table api_table)
 {
     /* clang-format off */
 
-    ELONA_LUA_API_BIND_FUNCTION("log", Debug_log);
-    ELONA_LUA_API_BIND_FUNCTION("report_error", Debug_report_error);
     ELONA_LUA_API_BIND_FUNCTION("enable_wizard", Debug_enable_wizard);
     ELONA_LUA_API_BIND_FUNCTION("disable_wizard", Debug_disable_wizard);
     ELONA_LUA_API_BIND_FUNCTION("is_wizard", Debug_is_wizard);
